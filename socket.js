31c88328c1e87abe7762ad5903119ef2551a3364
Ashish48Maurya <maurya.48.ashish@gmail.com>
2025-01-05 11:03:42 +0530

    ok

--- socket.js
+++ socket.js
@@ -5,35 +5,41 @@ import http from "http";
 export const app = express();
 export const server = http.createServer(app);
 
-const userSocketMap = {};
-
 const io = new Server(server, {
     cors: {
-        origin: process.env.FRONTEND_URI,
-        credentials: true,
+        origin: process.env.FRONTEND_URI
     },
 });
 
-io.on("connection", (socket) => {
-    const userId = socket.handshake.query.userId;
+const users = [];
+function userJoin(id, userId, room) {
+    const user = { id, userId, room };
+    users.push(user);
+    return user;
+}
 
-    if (userId) {
-        userSocketMap[userId] = socket.id;
+function userLeave(id) {
+    const index = users.findIndex(user => user.id === id);
+    if (index !== -1) {
+        return users.splice(index, 1)[0];
     }
+}
 
-    socket.on("joinRoom", (room) => {
-        socket.join(room);
-        const roomSockets = io.sockets.adapter.rooms.get(room) || new Set();
-        const usersInRoom = Array.from(roomSockets).map((socketId) =>
-            Object.keys(userSocketMap).find((key) => userSocketMap[key] === socketId)
-        );
-        io.to(room).emit("roomUsers", usersInRoom);
-    });
+function getRoomUsers(room) {
+    return users.filter(user => user.room === room)
+}
+
+io.on("connection", (socket) => {
+    socket.on("joinRoom", ({ userId, room }) => {
+        const user = userJoin(socket.id, userId, room);
+        socket.join(user?.room);
+        io.to(room).emit("roomUsers", { room, users: getRoomUsers(room) });
+    })
 
     socket.on("sendMessage", (messageData) => {
         const { room, message, createdAt, user } = messageData;
         if (room) {
-            io.to(room).emit("receiveMessage", { message, createdAt, user, senderId: socket.id });
+            io.to(room).emit("receiveMessage", { message, room, createdAt, user, senderId: socket.id });
         } else {
             console.error("Room not specified for sendMessage");
         }
@@ -47,10 +53,10 @@ io.on("connection", (socket) => {
         io.to(room).emit("userStoppedTyping", { userId });
     });
 
-    socket.on("disconnect", () => {
-        if (userId && userSocketMap[userId] === socket.id) {
-            delete userSocketMap[userId];
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+        if (user) {
+            io.to(user?.room).emit("roomUsers", { room: user.room, users: getRoomUsers(user.room) });
         }
-       //handle roomUsers after socket disconnect
-    });
+    })
 });
